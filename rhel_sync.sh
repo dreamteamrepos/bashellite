3f7ec1cf585821a7f675e610d51cb652521d7817
Eric Lake <ericlake@gmail.com>
2018-04-13 11:25:50 -0500

    add pypi and rhel sync scripts

---
+++ rhel_sync.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DEST_DIR=/mirror1/
+
+declare -a repos=(
+# Base RHEL
+ "rhel-7-server-rpms"
+ "rhel-7-server-rh-common-rpms"
+ "rhel-7-server-extras-rpms"
+ "rhel-7-server-optional-rpms"
+ "rhel-7-server-supplementary-rpms"
+ "rhel-7-server-fastrack-rpms"
+ "rhel-7-server-optional-fastrack-rpms"
+ "rhel-server-rhscl-7-rpms"
+# Openstack Packages
+ "rhel-ha-for-rhel-7-server-rpms"
+ "rhel-7-server-openstack-10-rpms"
+ "rhel-7-server-openstack-10-tools-rpms"
+ "rhel-7-server-openstack-10-optools-rpms"
+ "rhel-7-server-openstack-10-devtools-rpms"
+# Ceph Packages
+ "rhel-7-server-rhceph-2-tools-rpms"
+ "rhel-7-server-rhceph-2-osd-rpms"
+ "rhel-7-server-rhceph-2-mon-rpms"
+ "rhel-7-server-rhscon-2-installer-rpms"
+)
+
+for repo in "${repos[@]}"; do
+  reposync -lnm -r $repo -e ${DEST_DIR}/cache -p ${DEST_DIR}
+  createrepo -v --workers 4 -c ${DEST_DIR}/cache --update -g comps.xml ${DEST_DIR}/${repo}/
+done
